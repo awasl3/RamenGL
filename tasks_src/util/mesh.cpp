32196f7f9c5b48497f08adf0907a97257f9fd9d1
awasl <awasl@hm.edu>
2026-04-26 13:59:02 +0000

    added util mesh class

---
+++ tasks_src/util/mesh.cpp
@@ -0,0 +1,39 @@
+#include <glad/glad.h>
+
+struct Mesh
+{
+
+    GLuint vbo, vao;
+    int count;
+
+    Mesh(const std::vector<Vertex>& verts) {
+        count = verts.size();
+        glCreateBuffers(1, &vbo);
+        glNamedBufferData(vbo, count * sizeof(Vertex), verts.data(), GL_STATIC_DRAW);
+        
+        glCreateVertexArrays(1, &vao);
+        glVertexArrayVertexBuffer(vao, 0, vbo, 0, sizeof(Vertex));
+        /* Position */
+        glVertexArrayAttribFormat(vao, 0, 3, GL_FLOAT, GL_FALSE, 0);
+        glEnableVertexArrayAttrib(vao, 0);
+        glVertexArrayAttribBinding(vao, 0, 0);
+        /* Normal */
+        glVertexArrayAttribFormat(vao, 1, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float));
+        glEnableVertexArrayAttrib(vao, 1);
+        glVertexArrayAttribBinding(vao, 1, 0);
+    }
+
+    void deleteBuffers() {
+        glDeleteBuffers(1, &vbo);
+        glDeleteVertexArrays(1, &vao);
+    }
+
+    void activate() {
+        glBindVertexArray(vao);
+    }
+
+    void draw() {
+        glDrawArrays(GL_TRIANGLES, 0, count);
+    }
+};
+
